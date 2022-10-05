15e6724901611dd48641e743142704ba282fca78
Singgi Project <singgimokodompit.it@gmail.com>
2022-10-06 03:50:46 +0800

    add

---
+++ delete-bap.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+if (!isset($_SESSION["login"])) {
+  header("Location:../../../auth/login");
+  exit;
+}
+
+require('../../../funct/functions.php');
+
+
+// SESSION USER LOGIN
+if (isset($_SESSION["login"])) {
+
+  $userSession = $_SESSION["username"];
+  $resultSession = $conn->query("SELECT * FROM tb_users WHERE username = '$userSession' ");
+  $rowSession = mysqli_fetch_assoc($resultSession);
+  $idSession = $rowSession["id"];
+}
+
+// DELETE/HAPUS DATA bap
+if (isset($_GET["id_del_bap"])) {
+
+  $id = $_GET["id_del_bap"];
+  if (deleteBAP($id) > 0) {
+    echo "
+    <script>
+      alert('Data berhasil di hapus!');
+      document.location.href = 'bap';
+    </script>";
+  } else {
+    echo "
+    <script>
+      alert('Data gagal di hapus!');
+      document.location.href = 'bap';
+    </script>";
+  }
+}
+
+
+// CEK LEVEL
+include("../../../include/check-level.php");
